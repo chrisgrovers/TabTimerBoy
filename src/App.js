4d46829e9624ed80ec4b6561bc21358777905b05
Chris Grovers <chris@tapclicks.com>
2017-06-21 23:26:38 -0700

    this is where I end for the night

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+/**
+  TODO:
+// Need to keep a queue of tabs set for alarm
+// Point default popup to index.js...?
+
+*/
+
+
+// Code to open a new window:
+/**
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+    chrome.tabs.create({ url: newURL });
+});
+*/
+
+
+console.log('hello');
 
 class App extends Component {
   render() {
@@ -13,6 +32,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.onClick}>Open Later</button>
       </div>
     );
   }
